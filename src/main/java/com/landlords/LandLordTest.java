54607ecbf1b9344cbd39d2b3292ee8ab62321c97
pjholland <pjholland2000@yahoo.co.uk>
2016-06-10 14:17:52 +0200

    added nagative test

--- src/main/java/com/landlords/LandLordTest.java
+++ src/main/java/com/landlords/LandLordTest.java
@@ -2,31 +2,118 @@ package com.landlords;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
 
-import org.hamcrest.Matchers;
+import com.landlords.model.Landlord;
+import io.restassured.http.ContentType;
+import org.junit.Before;
 import org.junit.Test;
-
-import  io.restassured.matcher.RestAssuredMatchers.*;
 
 
 public class LandLordTest {
 
+    @Before
+    public void init(){
+        baseURI = "http://localhost:8080";
+    }
+
+
     @Test
     public void getLandLords() {
-
-        baseURI = "http://localhost:8080";
 
         when()
                 .get("/landlords")
                 .then()
                 .statusCode(200)
-        .body("", is(empty()));
+                .body("", is(empty()));
     }
 
     @Test
-    public void postLandLord01(){
+    public void postLandLord01() {
 
+        Landlord landlord = new Landlord("Paul", "Stevens");
+
+        String id = given()
+                .contentType(ContentType.JSON)
+                .body(landlord)
+                .when()
+                .post("/landlords")
+                .then()
+                .statusCode(201)
+                .body("firstName", is(landlord.getFirstName()))
+                .body("lastName", is(landlord.getLastName()))
+                .body("trusted", is(false))
+                .body("apartments", is(empty()))
+                .extract()
+                .path("id");
+        given()
+                .pathParam("id", id)
+                .when()
+                .get("/landlords/{id}")
+                .then()
+                .statusCode(200)
+                .body("id", is(id))
+                .body("firstName", is(landlord.getFirstName()))
+                .body("lastName", is(landlord.getLastName()))
+                .body("trusted", is(false))
+                .body("apartments", is(empty()));
 
     }
+
+    @Test
+    public void postLandLord02() {
+
+        Landlord landlord = new Landlord("Paul", "Stevens", true);
+
+        String id = given()
+                .contentType(ContentType.JSON)
+                .body(landlord)
+                .when()
+                .post("/landlords")
+                .then()
+                .statusCode(201)
+                .body("firstName", is(landlord.getFirstName()))
+                .body("lastName", is(landlord.getLastName()))
+                .body("trusted", is(true))
+                .body("apartments", is(empty()))
+                .extract()
+                .path("id");
+        given()
+                .pathParam("id", id)
+                .when()
+                .get("/landlords/{id}")
+                .then()
+                .statusCode(200)
+                .body("id", is(id))
+                .body("firstName", is(landlord.getFirstName()))
+                .body("lastName", is(landlord.getLastName()))
+                .body("trusted", is(false))
+                .body("apartments", is(empty()));
+
+    }
+
+
+
+
+    @Test
+    public void postLandLordNegative01() {
+
+        Landlord landlord = new Landlord("", "");
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(landlord)
+                .when()
+                .post("/landlords")
+                .then()
+                .statusCode(400)
+                .body("message", is("Fields are with validation errors"))
+                .body("fieldErrorDTOs[0].fieldName", is("firstName"))
+                .body("fieldErrorDTOs[0].fieldError",is("First name can not be empty"))
+                .body("fieldErrorDTOs[1].fieldName", is("lastName"))
+                .body("fieldErrorDTOs[1].fieldError",is("Last name can not be empty"));
+
+    }
+
+
+
 }
