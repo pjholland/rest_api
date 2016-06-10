48464039b454513094a7158e03e4420e58c84e26
pjholland <pjholland2000@yahoo.co.uk>
2016-06-10 17:18:04 +0200

    added further tests

--- src/main/java/com/landlords/LandLordTest.java
+++ src/main/java/com/landlords/LandLordTest.java
@@ -91,9 +91,6 @@ public class LandLordTest {
 
     }
 
-
-
-
     @Test
     public void postLandLordNegative01() {
 
@@ -113,6 +110,85 @@ public class LandLordTest {
                 .body("fieldErrorDTOs[1].fieldError",is("Last name can not be empty"));
 
     }
+
+    @Test
+    public void putLandlord(){
+
+
+        Landlord landlord = new Landlord("Mary", "Horse", true);
+
+        String id = given()
+                .contentType(ContentType.JSON)
+                .body(landlord)
+                .when()
+                .post("/landlords")
+                .then()
+                .statusCode(201)
+                .extract()
+                .path("id");
+
+        Landlord landLordForUpdate = new Landlord("Mary", "Donkey");
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(landLordForUpdate)
+                .pathParam("id", id)
+                .when()
+                .put("/landlords/{id}")
+                .then()
+                .statusCode(200)
+                .body("message", is("LandLord with id: "+id+" successfully updated"));
+
+        given()
+                .pathParam("id", id)
+                .when()
+                .get("/landlords/{id}")
+                .then()
+                .statusCode(200)
+                .body("firstName", is(landLordForUpdate.getFirstName()))
+                .body("lastName", is(landLordForUpdate.getLastName()))
+                .body("trusted", is(false));
+
+
+    }
+
+    @Test
+    public void deleteLandlord01() {
+
+        Landlord landlord = new Landlord("Bill", "Stone");
+
+        String id = given()
+                .contentType(ContentType.JSON)
+                .body(landlord)
+                .when()
+                .post("/landlords")
+                .then()
+                .statusCode(201)
+                .extract()
+                .path("id");
+        given()
+                .pathParam("id", id)
+                .when()
+                .delete("/landlords/{id}")
+                .then()
+                .statusCode(200)
+                .body("message", is("LandLord with id: "+id+" successfully deleted"));
+
+        given()
+                .pathParam("id", id)
+                .when()
+                .get("/landlords/{id}")
+                .then()
+                .statusCode(404)
+                .body("message", is("There is no landlord with id: " +id));
+
+
+
+
+    }
+
+
+
 
 
 
--- src/main/java/com/landlords/model/landlord.java
+++ src/main/java/com/landlords/model/landlord.java
@@ -1,4 +1,48 @@
 package com.landlords.model;
 
-public class landlord {
+public class Landlord {
+
+    private String  firstName;
+    private String  lastName;
+    private Boolean trusted;
+
+    public Landlord(){
+    }
+
+    public  Landlord(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName  = lastName;
+    }
+
+    public  Landlord(String firstName, String lastName, Boolean trusted){
+        this.firstName = firstName;
+        this.lastName  = lastName;
+        this.trusted   = trusted;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(){
+        this.firstName = firstName;
+    }
+
+    public  String getLastName(){
+        return lastName;
+    }
+
+    public void setLastName(){
+        this.lastName = lastName;
+    }
+
+    public Boolean getTrusted(){
+        return trusted;
+    }
+
+    public void  setTrusted(Boolean trusted){
+        this.trusted = trusted;
+    }
+
+
 }
